64e42196ad9052c7ce3078517b72893247084833
Samuel Kreuz <samuel.kreuz@live.com>
2022-12-16 15:12:38 +0100

    day 07 done

---
+++ 2022_AdventOfCode/Day07/Folder.cs
@@ -0,0 +1,45 @@
+﻿namespace Day07
+{
+    internal class Folder
+    {
+        public string Name { get; }
+        public Folder Parent { get; }
+        private List<Folder> Subfolders = new List<Folder>();
+        private Dictionary<string, int> Files = new Dictionary<string, int>();
+        public int Size { get; set; } = 0;
+
+        public Folder(string name, Folder parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+
+        public Folder AddSubfolder(string name)
+        {
+            Folder subfolder = Subfolders.FirstOrDefault(x => x.Name == name);
+
+            if(subfolder == null)
+            {
+                subfolder = new Folder(name, this);
+                Subfolders.Add(subfolder);
+            }
+
+            return subfolder;
+        }
+
+        public void AddFile(string name, int size)
+        {
+            Files.Add(name, size);
+        }
+
+        public void UpdateFolderSize() 
+        {
+            Size += Files.Sum(x => x.Value);
+            Subfolders.ForEach(x => x.UpdateFolderSize());
+            Subfolders.ForEach(x => Size += x.Size);
+        }
+
+    }
+
+
+}
---
+++ 2022_AdventOfCode/Day07/Program.cs
@@ -0,0 +1,68 @@
+﻿//Part 1: 2h
+using Day07;
+string path = "../../../input/input.txt";
+var lines = File.ReadAllLines(path);
+
+Folder uppestFolder = null;
+Folder currentFolder = null;
+List<Folder> allFolders = new List<Folder>();
+
+foreach (var line in lines)
+{
+    var lineStrings = line.Split(' ');
+
+    // Move into folder
+    if (line.StartsWith("$ cd") && lineStrings[2] != "..")
+    {
+        string name = lineStrings[2].Trim();
+        if (uppestFolder == null)
+        {
+            uppestFolder = new Folder(name, null);
+            currentFolder = uppestFolder;
+            allFolders.Add(uppestFolder);
+        }
+        else
+        {
+            Folder subfolder = currentFolder.AddSubfolder(name);
+            currentFolder = subfolder;
+            allFolders.Add(subfolder);
+        }
+    }
+
+    // Add Dir in Current
+    if (line.StartsWith("dir"))
+    {
+        string name = lineStrings[1];
+        currentFolder.AddSubfolder(name);
+    }
+
+    // Add File
+    if (!line.StartsWith("dir") && !line.StartsWith('$'))
+    {
+        int size = int.Parse(lineStrings[0].Trim());
+        string name = lineStrings[1];
+        currentFolder.AddFile(name, size);
+    }
+
+    // Move out
+    if (lineStrings.Length >= 3 && lineStrings[2] == "..")
+    {
+        currentFolder = currentFolder.Parent;
+    }
+
+}
+
+uppestFolder.UpdateFolderSize();
+var sum = allFolders.Where(x => x.Size <= 100000).Sum(y => y.Size);
+Console.WriteLine("Part 1: " + sum);
+
+
+//Part 2: 5min
+int unusedSpace = 70000000 - uppestFolder.Size;
+int neededSpace = 30000000 - unusedSpace;
+
+var folder = allFolders.Where(y => y.Size >= neededSpace)
+                       .OrderBy(x => x.Size)
+                       .FirstOrDefault();
+
+Console.WriteLine("Part 2: " + folder.Size);
